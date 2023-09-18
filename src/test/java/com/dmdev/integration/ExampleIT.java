a51a9e87630c49fd1e817a14cd0b1d58607eabb1
e.ivannikov <e.ivannikov@itfbgroup.ru>
2023-09-18 22:39:24 +0500

    main: Integration tests

---
+++ src/test/java/com/dmdev/integration/ExampleIT.java
@@ -0,0 +1,11 @@
+package com.dmdev.integration;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ExampleIT {
+    @Test
+    public void test() {
+        Assert.assertTrue(true);
+    }
+}
